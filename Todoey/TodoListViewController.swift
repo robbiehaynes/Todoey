556155698964b0cdecf1e536a68e6bfd1b5f1a51
Robert Haynes <robhaynes0420@gmail.com>
2024-11-07 12:22:43 +0000

    Add items alert and gitignore
    Added an alert popup to add items to the list.
    Added a gitignore to the project.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let items = ["Buy milk", "Go to the gym", "Clean the house"]
+    var items = ["Buy milk", "Go to the gym", "Clean the house"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +39,27 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // Added item
+            self.items.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            textField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
     
 }
 
